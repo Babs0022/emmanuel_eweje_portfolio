6834e1e0194a529116439fb8bd2e6101748be97e
Emmanuel Gbenga Eweje <ewejeemmanuel90@gmail.com>
2025-10-07 22:10:23 +0000

    starting build "49b94cc0-46b2-4272-909b-9032c4816972" FETCHSOURCE Fetchi

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -12,7 +12,7 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/com
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { useToast } from '@/hooks/use-toast';
-import { uploadArtwork, uploadProfilePicture, deleteArtwork } from '../actions';
+import { uploadArtwork, uploadProfilePicture, deleteArtwork } from '@/app/actions';
 import { Skeleton } from '@/components/ui/skeleton';
 import { db } from '@/lib/firebase';
 import { collection, onSnapshot, query, orderBy } from 'firebase/firestore';
